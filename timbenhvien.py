b84c2a947c176a5a7b0775670ef1c64de37baae0
Ha-Huynh-Anh <huynhanh.ha94@gmail.com>
2021-07-03 18:51:34 +0700

    ksc

--- timbenhvien.py
+++ timbenhvien.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
 # from gsheetsdb import connect
 import gspread
 # from oauth2client.service_account import ServiceAccountCredentials
@@ -31,6 +32,90 @@ def get_data():
     worksheet1 = sh.get_worksheet(0)
     df_1 = pd.DataFrame(worksheet1.get_all_records())
     return df_1
+#sym_df
+@st.cache
+def get_sym_df(url1):
+  sym_df = pd.read_csv(url1)
+  sym_df = sym_df.loc[:,('eng','viet')]
+  return sym_df
+
+#dis_df
+@st.cache
+def get_dis_df(url1):
+  dis_df = pd.read_csv(url1)
+  dis_df = dis_df.loc[:,('id_diseases', 'eng', 'viet', 'bv_tuong_ung')]
+  return dis_df
+
+#train_df
+@st.cache(allow_output_mutation=True)
+def get_train_df(url1):
+  train_df = pd.read_csv(url1)
+  train_df['vector'] = train_df.iloc[:,-133:-1].apply(lambda x: list(x), axis=1) ## sau nay nen tong quat hoa cac con so 133, 1, ...
+  return train_df
+
+#load data_chan doan benh
+url_train_df = 'https://raw.githubusercontent.com/Ha-Huynh-Anh/Timbenhvien_test/main/data/train_df.csv'
+url_dis_df = 'https://raw.githubusercontent.com/Ha-Huynh-Anh/Timbenhvien_test/main/data/diseases_df.csv'
+url_sym_df = 'https://raw.githubusercontent.com/Ha-Huynh-Anh/Timbenhvien_test/main/data/sym_df.csv'
+
+sym_df = get_sym_df(url_sym_df)
+dis_df = get_dis_df(url_dis_df)
+train_df = get_train_df(url_train_df)
+# function to translate sym vie - eng:
+def trans_sym(list_sym):
+    """
+    translate sym from list vie to list eng
+    input: ['Yếu mệt','Da nổi mẩn']
+    output: ['lethargy', 'nodal_skin_eruptions']
+    """
+    list_eng = []
+    for i in list_sym:
+        if i in list(sym_df.viet):
+            index = list(sym_df.viet).index(i)
+            list_eng.append(list(sym_df.eng)[index])
+    return list_eng
+
+# sym_to_vector(sym_input_eng)
+# convert to array format
+def sym_to_vector(list_input_sym):
+    #  to convert sym to vector
+    sym_input_vector = []    
+    for i in list(sym_df.eng): 
+        if i in list_input_sym:
+            sym_input_vector.append(1)
+        else:
+            sym_input_vector.append(0)
+    return sym_input_vector
+
+# convert all into list
+def get_disease(train_df, input_array):
+    
+    # cal similarity
+    train_df['similar'] = train_df.vector.apply(lambda x: cosine_similarity(input_array,np.array([x]))[0][0])
+
+    # disease result
+    dis_result = train_df[train_df['similar'] > 0]
+    return dis_result.groupby('prognosis').max('similar').iloc[:,-1].sort_values(ascending=False)    
+
+list_disease_eng = dis_df.eng.to_list()
+list_disease_vie = dis_df.viet.to_list()
+list_disease_id = dis_df.id_diseases.to_list()
+
+# convert disease to vie
+def disease_to_vie(dis_eng):
+    if dis_eng in list_disease_eng:
+        index = list_disease_eng.index(dis_eng)
+        return list_disease_vie[index]
+def disease_to_id(dis_eng):
+    if dis_eng in list_disease_eng:
+        index = list_disease_eng.index(dis_eng)
+        return list_disease_id[index]
+#Lay ID benh vien tuong ung voi ID benh
+def get_hopital_list(dis_id):
+    bv_id = dis_df[dis_df.id_diseases == dis_id].bv_tuong_ung
+    bv_id = list(bv_id)[0]
+    bv_id = bv_id.split(sep = ';')
+    return bv_id
 
 df_1 = get_data()
 st.sidebar.subheader('Bạn đang tìm bệnh viện theo tiêu chí:')
@@ -45,13 +130,29 @@ elif option1 == 'Khám sức khỏe cho người nước ngoài':
     df_result = df_1[df_1.iloc[:,8].str.contains('Thực hiện')].iloc[:,[1,3,4,8]]
 elif option1 == 'Khám sức khỏe để xuất ngoại':
     df_result = df_1[df_1.iloc[:,6] == 'x'].iloc[:,[1,3,4,8]]
+elif option1 == 'Cần tư vấn bệnh viện theo triệu chứng':
+    sym_input_vie = st.multiselect('Các triệu chứng của bạn', sym_df.viet.values)
+    # translate vie into eng
+    sym_input_eng = trans_sym(sym_input_vie)
+    #convert to vector
+    input_array = np.array([sym_to_vector(sym_input_eng)])
+    output_disease_eng =  get_disease(train_df,input_array)
+    output_disease_eng = output_disease_eng.reset_index()
+    # create table
+    output_disease_eng['viet'] = output_disease_eng.prognosis.apply(lambda x: disease_to_vie(x))
+    output_disease_eng['id'] = output_disease_eng.prognosis.apply(lambda x: disease_to_id(x))
+    st.markdown(':hospital: Bạn có thể cần được khám về ')
+    for i in output_disease_eng.index:
+        see_hospital = st.checkbox(output_disease_eng.iloc[i,2])
+        if see_hospital:
+            id_bv = get_hopital_list(output_disease_eng.iloc[i,3])
 
-if option1 != 'Vui lòng lựa chọn ở các ô bên dưới':
+if option1 not in ['Vui lòng lựa chọn ở các ô bên dưới','Cần tư vấn bệnh viện theo triệu chứng'] :
     st.dataframe(df_result)
 
-check_box = st.checkbox("Tìm các bệnh viện gần nhất")
+check_box = st.sidebar.checkbox("Tìm các bệnh viện gần nhất")
 if check_box:
-    with st.form('bv_gan'):
+    with st.sidebar.form('bv_gan'):
         diachi_user = st.text_input("Địa chỉ của bạn", 'ví dụ: Lê Hồng Phong, Hồ Chí Minh hoặc Lottemart Cộng Hòa')
         submitted = st.form_submit_button("Tìm")
 
